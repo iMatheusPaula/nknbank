f5fdea975a583c2311277064ccffb65358f4fc77
iMatheusPaula <imatheuspaula@gmail.com>
2024-07-24 14:05:48 -0300

    form update

--- index.php
+++ index.php
@@ -3,38 +3,46 @@ require __DIR__ . "/vendor/autoload.php";
 $mysql = new \Source\MySQL();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $handler = new \Source\FormHandler();
-    $handler->getForm();
-    //header('');
-    //die();
+    echo $handler->getForm();
 }
+//exit(header('Location: https://www.nknbank.com.br/'));
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>NKN Bank</title>
+    <title>Cadstro NKN Bank</title>
+    <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
-    <form method="post">
-        <label for="name">Nome:</label>
-        <input type="text" id="name" name="name">
-
-        <label for="birthday">Data de nascimento:</label>
-        <input type="date" id="birthday" name="birthday">
-
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email" >
-
-        <label for="password">Senha:</label>
-        <input type="password" id="password" name="password" >
-
-        <label for="confirmPassword">Confirmação de senha:</label>
-        <input type="password" id="confirmPassword" name="confirmPassword" >
-
-        <label for="whatsapp">Whatsapp:</label>
-        <input type="tel" id="whatsapp" name="whatsapp">
-
-        <button type="submit">Cadastrar</button>
-    </form>
+    <div class="form-box">
+        <form method="post">
+            <h1>Cadastro</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Nome" required>
+            </div>
+            <div class="input-box">
+                <input type="date" name="birthday" >
+            </div>
+            <div class="input-box">
+                <input type="text" name="email" placeholder="E-mail" required>
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Senha" required>
+            </div>
+            <div class="input-box">
+                <input type="password" name="confirmPassword" placeholder="Confirmação da senha" required>
+            </div>
+            <div class="input-box">
+                <input type="text" name="whatsapp" placeholder="Whatsapp">
+            </div>
+            <div class="hasErrors">
+<!--                --><?php //if(){ ?>
+<!--                <span>print_r </span>-->
+<!--                --><?php //} ?>
+            </div>
+            <button type="submit" class="btnSubmit">Cadastrar</button>
+        </form>
+    </div>
 </body>
 </html>
--- source/FormHandler.php
+++ source/FormHandler.php
@@ -2,34 +2,52 @@
 
 namespace Source;
 
+use DateTime;
+use DateTimeZone;
+use PDOException;
+
 class FormHandler extends MySQL
 {
     public function getForm()
     {
         $formData = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-        if($formData['name'] == '') echo "Preencha o nome!";
-        if($formData['birthday'] == '') echo "Preencha sua data de nascimento";
-        if($formData['email'] == '' || !filter_var($formData['email'], FILTER_VALIDATE_EMAIL)) echo "e-mail invalido";
-        if($formData['password'] == '') echo "Preencha a senha!";
-        if($formData['confirmPassword'] != $formData['password']) echo "As senhas não conferem";
-        if(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{6}$/', $formData['password'])){
-            echo 'Senha não atende os requisitos';
-        }
-        if($this->checkAge($formData['birthday'])) echo "Você precisa ser maior de 18 anos";
-        if($this->checkEmail($formData['email'])) echo "E-mail já cadastrado";
 
-        $query = "INSERT INTO lead (nome, datanascimento, email, senha, whatsapp, datalog) VALUES (?,?,?,?,?,?)";
-        $stmt= $this->connection->prepare($query);
-        $stmt->execute([
-            $formData['name'],
-            $formData['birthday'],
-            $formData['email'],
-            $formData['password'],
-            $formData['whatsapp'],
-            new \DateTime()
-        ]);
+        if($formData['name'] == '') return "Preencha o nome!";
+        if($formData['birthday'] == '') return "Preencha sua data de nascimento";
+        if($formData['email'] == '' || !filter_var($formData['email'], FILTER_VALIDATE_EMAIL)) return "e-mail invalido";
+        if($formData['password'] == '') return "Preencha a senha!";
+        if($formData['confirmPassword'] != $formData['password']) return "As senhas não conferem";
+        if(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{6,}$/', $formData['password'])){
+            return 'Senha não atende os requisitos';
+        }
+        if($this->checkAge($formData['birthday'])) return "Você precisa ser maior de 18 anos";
+        if($this->checkEmail($formData['email'])) return "E-mail já cadastrado";
+        $setTimeZone = new DateTimeZone('America/Sao_Paulo');
+        $dataLog = new DateTime();
+
+        try{
+//            $query = "INSERT INTO lead (nome, datanascimento, email, senha, whatsapp, datalog) VALUES (?,?,?,?,?,?)";
+//            $stmt= $this->connection->prepare($query);
+//            $stmt->execute([
+//                $formData['name'],
+//                $formData['birthday'],
+//                $formData['email'],
+//                $formData['password'],
+//                $formData['whatsapp'],
+//                $dataLog->format("Y-m-d H:i:s")
+//            ]);
+//            $mailMesssage = "Olá {$formData['name']}, seu cadastro no banco NKN foi realizado com sucesso.";
+//            mail(
+//                $formData['email'],
+//                "Bem-Vindo ao NKN Bank!",
+//                $mailMesssage
+//            );
+            return "Tudo certo";
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
     }
-    private function checkAge(string $birthday): bool
+    public function checkAge(string $birthday): bool
     {
         $dateNow = new DateTime();
         $birthday = new DateTime($birthday);
@@ -37,7 +55,7 @@ class FormHandler extends MySQL
         if($dateNow->diff($birthday)->y >= 18) return false;
         return true;
     }
-    private function checkEmail(string $email): bool
+    public function checkEmail(string $email): bool
     {
         $query = "SELECT email FROM lead WHERE email = ?";
         $stmt= $this->connection->prepare($query);
